c6a1536a7df2f0bc234521b6b774d6b1be6d6402
Fernando <fernando.faustino.f@gmail.com>
2022-05-19 11:03:44 -0300

    Três aulas

---
+++ AlunosApi/AlunosApi/Services/IAlunoService.cs
@@ -0,0 +1,15 @@
+﻿using AlunosApi.Models;
+
+namespace AlunosApi.Services
+{
+    public interface IAlunoService
+    {
+        Task<IEnumerable<Aluno>> GetAlunos();
+        Task<Aluno> GetAluno(int id);
+        Task<IEnumerable<Aluno>> GetAlunosByNome(string nome);
+        Task CreatAluno(Aluno aluno);
+        Task UpdateAluno(Aluno aluno);
+        Task DeleteAluno(Aluno aluno);
+
+    }
+}
